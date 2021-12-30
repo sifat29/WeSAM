8f933ea57f2dfc6a760cfd79bb0717ff8901516b
sifat29 <96700012+sifat29@users.noreply.github.com>
2021-12-30 14:07:30 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>wesam</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://kit.fontawesome.com/97441ecf66.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <header>
+      <div class="action_bar d-none d-lg-block">
+          <div class="container">
+              <div class="row">
+                  <div class="col-xl-3 col-lg-4" style="text-align: right">
+                  </div>
+                  <div class="col-xl-7 col-lg-6 bordered">
+                  </div>
+                  <div class="col-lg-2 social">
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+         <a class="navbar-brand" href="#"><img class="logo-img" src="images/logo.png"></a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+           </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                 <ul class="navbar-nav ml-auto">
+                   <li class="nav-item active">
+                       <a class="nav-link" href="home2.php">HOME<span class="sr-only">(current)</span></a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="project.php">PROJECTS</a>
+                   </li>
+                   <li class="nav-item dropdown view">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        MEMBERS
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="sadia_anjum.html" target="_blank">SADIA ANJUM</a>
+                        <a class="dropdown-item" href="fahim_hossain.html" target="_blank">FAHIM HOSSAIN</a>
+                        <a class="dropdown-item" href="rahnuma_mahzabin.html" target="_blank">RAHNUMA MAHZABIN</a>
+                        <a class="dropdown-item" href="tasmuna.html" target="_blank">TASMUNA ISLAM</a>
+                        </div>
+                  </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="contact.php">CONTACT US</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="message.php">MESSAGES</a>
+                   </li>
+                </ul>
+            </div>
+            </nav>
+      </div>
+  </header>
+   <section id="s1">
+       <div class="container-fluid">
+           <div class="row">
+               <div class="col-lg-3"></div>
+               <div class="col-lg-6">
+                   <span class="wesam"><b>WESAM</b></span>
+               </div>
+               <div class="col-lg-3"></div>
+           </div><br>
+           <div class="row">
+              <div class="col-lg-3"></div>
+               <div class="col-lg-1">
+                   <hr>
+               </div>
+               <div class="col-lg-3">
+                   <p style="font-family:font-family: courier; font-size: 120%;">“The beautiful thing about learning is that no one can take it away from you” – B. B. King.</p>
+               </div>
+               <div class="col-lg-4">
+					<button class="con"><b><a class="log-button" href="logout.php">Logout</a></b></button>
+
+               </div>
+               <div class="col-lg-1"></div>
+
+           </div>
+       </div>
+   </section>
+   <section id="s3">
+       <div class="container">
+           <div class="row">
+               <div class="col-lg-6" style="background-image: url('address.jpg'); border: solid 3px; border-color: hsl(0, 100%, 25%)">
+                   <p class="p3"><b>Contact Us</b></p>
+                   <img src="images/section3%20line%20-%20Copy.png" class="img1">
+                   <p class="p4" style="font-family: Times New Roman, Times, serif; font-size: 130%;"><strong>Address: </strong>Mohammadpur, Dhaka, Bangladesh</p>
+                   <p class="p5" style="font-family: Times New Roman, Times, serif; font-size: 130%;"><strong>Phone: </strong>911-5242, 913-8243, 912-7977</p>
+                   <p class="p5" style="font-family: Times New Roman, Times, serif; font-size: 130%;"><strong>Fax: </strong>88-02-9660610</p>
+                   <p class="p5" style="font-family: Times New Roman, Times, serif; font-size: 130%;"><strong>Email: </strong>wesam2021@gmail.com</p>
+               </div>
+               <div class="col-lg-6">
+                 <section class="section pt-0">
+                   <div style="margin-left: 10%;"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.613311820173!2d90.34848631481547!3d23.761164984584067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755bf623cc551d3%3A0x226bd6dcebde4420!2sU.L.A.B%20Permanent%20Campus!5e0!3m2!1sen!2sbd!4v1630405319683!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+                 </section>
+               </div>
+           </div>
+       </div>
+   </section>
+   <footer>
+      <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-8 mb-5 mb-lg-0">
+                <span class="mb-2 fot-add" style="color:#ff8800"><b>Recommend</b></span>
+              <ul class="address">
+                <li class="mb-2">WESAM</li>
+                <li class="mb-2">+8801912321412</li>
+              </ul>
+            </div>
+        </div>
+        <div class="row Copyright">
+            <div class="col-lg-5 col-md-10 d-sm-none d-md-block">
+                <span class="span2">Copyright &copy; 2021 | Web Technology</span>
+            </div>
+        </div>
+    </div>
+   </footer>
+    <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,164 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>wesam</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://kit.fontawesome.com/97441ecf66.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <header>
+      <div class="action_bar d-none d-lg-block">
+          <div class="container">
+              <div class="row">
+                  <div class="col-xl-3 col-lg-4" style="text-align: right">
+                  </div>
+                  <div class="col-xl-7 col-lg-6 bordered">
+                  </div>
+                  <div class="col-lg-2 social">
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+         <a class="navbar-brand" href="#"><img class="logo-img" src="images/logo.png"></a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+           </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                 <ul class="navbar-nav ml-auto">
+                   <li class="nav-item active">
+                       <a class="nav-link" href="home.php">HOME<span class="sr-only">(current)</span></a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="login.php">PROJECTS</a>
+                   </li>
+                   <li class="nav-item dropdown view">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        MEMBERS
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="login.php" target="_blank">SADIA ANJUM</a>
+                        <a class="dropdown-item" href="login.php" target="_blank">FAHIM HOSSAIN</a>
+                        <a class="dropdown-item" href="login.php" target="_blank">RAHNUMA MAHZABIN</a>
+                        <a class="dropdown-item" href="login.php" target="_blank">TASMUNA ISLAM</a>
+                        </div>
+                  </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="login.php">CONTACT US</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="login.php">MESSAGES</a>
+                   </li>
+                </ul>
+            </div>
+            </nav>
+      </div>
+  </header>
+   <section id="s1">
+       <div class="container-fluid">
+           <div class="row">
+               <div class="col-lg-3"></div>
+               <div class="col-lg-6">
+                   <span class="wesam"><b>WESAM</b></span>
+               </div>
+               <div class="col-lg-3"></div>
+           </div><br>
+           <div class="row">
+              <div class="col-lg-3"></div>
+               <div class="col-lg-1">
+                   <hr>
+               </div>
+               <div class="col-lg-3">
+                   <p style="font-family:font-family: courier; font-size: 120%;">“The beautiful thing about learning is that no one can take it away from you” – B. B. King.</p>
+               </div>
+               <div class="col-lg-4">
+                    <button class="con"><b><a class="log-button" href="login.php">Login</a></b></button>
+					          <button class="con"><b><a class="log-button" href="register.php">Sign-up</a></b></button>
+
+               </div>
+               <div class="col-lg-1"></div>
+
+           </div>
+       </div>
+   </section>
+   <section id="s3">
+       <div class="container">
+           <div class="row">
+               <div class="col-lg-6">
+                   <p class="p3"><b>About WESAM</b></p>
+                   <img src="images/section3%20line%20-%20Copy.png" class="img1">
+                   <p class="p4" style="font-family: Times New Roman, Times, serif; font-size: 120%;">It is a personal website about us</p>
+                   <p class="p5" style="font-family: Times New Roman, Times, serif; font-size: 120%;">This website contains our personal information. In this website we have share our projects that we have done at different time in our graduation.</p>
+                   <p class="p5" style="font-family: Times New Roman, Times, serif; font-size: 120%;">Anyone who wants customize project, who can contact with us. We are interested to do customize project for user. We are excited to help our users on their journey.</p>
+               </div>
+               <div class="col-lg-6">
+                   <img src="images/book1.jpg" class="img-fluid foco">
+               </div>
+           </div>
+       </div>
+   </section>
+   <footer>
+      <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-8 mb-5 mb-lg-0">
+                <span class="mb-2 fot-add" style="color: #ff8800"><b>Contact Us</b></span>
+              <ul class="address">
+                <li class="mb-2">WESAM</li>
+                <li class="mb-2">+8801912321412</li>
+                <li class="mb-2">wesam2021@gmail.com</li>
+              </ul>
+            </div>
+        </div>
+        <div class="row Copyright">
+            <div class="col-lg-5 col-md-10 d-sm-none d-md-block">
+                <span class="span2">Copyright &copy; 2021 | Web Technology</span>
+            </div>
+        </div>
+    </div>
+   </footer>
+    <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function() {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					head.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					head.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function(msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			console.log('Live reload enabled.');
+		})();
+	}
+	// ]]>
+</script>
+</body>
+</html>
